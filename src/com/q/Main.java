aad252c350183cc7b931f9276bc03216af968bad
Q. Soerokarso <quincysoeroakrso@novi-education.nl>
2020-09-21 11:41:22 +0200

    GitIntro lalal

--- src/com/q/Main.java
+++ src/com/q/Main.java
@@ -3,7 +3,7 @@ package com.q;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hi, allemaal");
         System.out.println("Nieuwe lijn");
         System.out.println("Hellooooo");
     }
